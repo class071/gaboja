d482ccfee514fa103c859cc600a3a8ac44b9ad84
we4sley <galaxy138742@gmail.com>
2022-07-09 13:08:16 +0900

    chore: 프로젝트 셋팅

---
+++ src/main/java/com/daily/gaboja/GabojaApplication.java
@@ -0,0 +1,13 @@
+package com.daily.gaboja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabojaApplication.class, args);
+	}
+
+}
