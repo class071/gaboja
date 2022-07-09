1f600881de17061264abf53bf1e38287db64845d
we4sley <galaxy138742@gmail.com>
2022-07-09 13:08:16 +0900

    chore: 프로젝트 셋팅

---
+++ src/test/java/com/daily/gaboja/GabojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.gaboja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
