a4ceb1e8d89c32b5bbd67545bbbb0e729f100b9d
gnoesnooj <306yyy@naver.com>
2022-07-12 23:49:55 +0900

    feat : JWT 구현

---
+++ src/main/java/com/daily/gaboja/jwt/config/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.daily.gaboja.jwt.config;
+
+import com.daily.gaboja.user.constant.UserRole;
+import com.daily.gaboja.user.domain.User;
+import com.daily.gaboja.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseGet(() -> new User(null,null, null, null, null, null, null));
+        Set<UserRole> userRole = new HashSet<>();
+        userRole.add(user.getRole());
+        return new CustomUserDetails(
+                String.valueOf(user.getEmail()),
+                userRole.stream().map(role -> role.getType())
+                        .map(SimpleGrantedAuthority::new).collect(Collectors.toSet())
+        );
+    }
+}
---
+++ src/main/java/com/daily/gaboja/jwt/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.daily.gaboja.jwt.config;
+
+import com.daily.gaboja.jwt.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenService tokenService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .formLogin().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/userinfo").authenticated()
+                .and()
+                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(tokenService, customUserDetailsService), UsernamePasswordAuthenticationFilter.class);
+    }
+}
