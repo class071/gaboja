274c3b2f3f17c200a310dad0c5288b93fd28f4ee
gnoesnooj <306yyy@naver.com>
2022-08-10 22:18:47 +0800

    refactor : cart 조회 response 객체 총액, 상품리스트 필드 추가

--- src/main/java/com/daily/gaboja/cart/dto/CartReadResponseDto.java
+++ src/main/java/com/daily/gaboja/cart/dto/CartReadResponseDto.java
@@ -1,7 +1,10 @@
 package com.daily.gaboja.cart.dto;
 
 import com.daily.gaboja.cart.domain.Cart;
+import com.daily.gaboja.cart.domain.ProductLine;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class CartReadResponseDto {
@@ -10,9 +13,15 @@ public class CartReadResponseDto {
 
     private Long userId;
 
-    public CartReadResponseDto convertEntityToDto(Cart cart) {
+    private List<ProductLine> productLines;
+
+    private int totalAmounts;
+
+    public CartReadResponseDto toDto(Cart cart) {
         this.cartId = cart.getId();
         this.userId = cart.getUser().getId();
+        this.productLines = cart.getProducts();
+        this.totalAmounts = cart.getTotalAmounts();
 
         return this;
     }
