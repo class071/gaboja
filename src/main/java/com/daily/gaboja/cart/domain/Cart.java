11e0ead2660419a4b6f6d119ba39e24b20ed8c4e
gnoesnooj <306yyy@naver.com>
2022-08-10 22:16:12 +0800

    feat : 장바구니에 담은 상품들을 가질 수 있는 ProductLine 구현

--- src/main/java/com/daily/gaboja/cart/domain/Cart.java
+++ src/main/java/com/daily/gaboja/cart/domain/Cart.java
@@ -4,8 +4,9 @@ import com.daily.gaboja.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import org.hibernate.annotations.ColumnDefault;
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,8 +21,27 @@ public class Cart {
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    //@CollectionTable(name = "CART_PRODUCTS", joinColumns = @JoinColumn(name = "PRODUCT_ID"))
+    @Embedded
+    private List<ProductLine> products;
+
+    @ColumnDefault("0")
+    private int totalAmounts; // product 연결 후 set 해주는 로직 필요
+
     @Builder
     public Cart(User user) {
         this.user = user;
+    }
+
+    public void changeProductLines(List<ProductLine> newLines) {
+        this.products = newLines;
+        this.totalAmounts = calculateTotalAmounts(newLines);
+    }
+
+    private int calculateTotalAmounts(List<ProductLine> lines) {
+        int sum = lines.stream()
+                .mapToInt(p -> p.getCost() * p.getQuantity()).sum();
+        return sum;
     }
 }
