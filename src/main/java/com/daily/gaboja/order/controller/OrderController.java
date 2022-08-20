29627d2aa27f721c377f0e66afe73621c23ace1e
gnoesnooj <306yyy@naver.com>
2022-08-20 21:58:20 +0800

    feat : order 컨트롤러 구현

---
+++ src/main/java/com/daily/gaboja/order/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.daily.gaboja.order.controller;
+
+import com.daily.gaboja.global.api.ApiResponse;
+import com.daily.gaboja.order.dto.ChangeInfoReqDto;
+import com.daily.gaboja.order.dto.OrderRequestDto;
+import com.daily.gaboja.order.dto.OrderResponseDto;
+import com.daily.gaboja.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/order/{orderId}")
+    public ApiResponse<OrderResponseDto> get(@PathVariable Long orderId){
+        return ApiResponse.success(HttpStatus.OK, orderService.get(orderId));
+    }
+
+    @PostMapping("/order/info")
+    public ApiResponse<OrderResponseDto> changeInfo(@RequestBody @Valid ChangeInfoReqDto changeInfoReqDto){
+        return ApiResponse.success(HttpStatus.OK, orderService.changeShippingInfo(changeInfoReqDto));
+    }
+
+    @PostMapping("/order")
+    public ApiResponse<OrderResponseDto> create(@RequestBody @Valid OrderRequestDto orderRequestDto){
+        return ApiResponse.success(HttpStatus.OK, orderService.sendCartToOrder(orderRequestDto));
+    }
+}
