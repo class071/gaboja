351ddfcf1fc08f18f0ba29484f78e5b3ea39fdc6
gnoesnooj <306yyy@naver.com>
2022-10-05 15:43:05 +0800

    feat : 결제 entity 구현

---
+++ src/main/java/com/daily/gaboja/payment/domain/Payment.java
@@ -0,0 +1,31 @@
+package com.daily.gaboja.payment.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderId;
+
+    private int totalAmount;
+
+    private String tid;
+
+    private String paymentState;
+}
