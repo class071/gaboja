581bd85bb11910cb40bc9f9672f8776fe94b967e
we4sley <galaxy138742@gmail.com>
2022-10-16 02:19:13 +0900

    feat: Product Entity 생성

---
+++ src/main/java/com/daily/gaboja/product/domain/Product.java
@@ -0,0 +1,44 @@
+package com.daily.gaboja.product.domain;
+
+import com.daily.gaboja.product.dto.ProductUpdateRequestDto;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column
+    @ColumnDefault("0")
+    private long stock;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column
+    @ColumnDefault("0")
+    private int price;
+
+    public void update(ProductUpdateRequestDto productUpdateRequestDto) {
+        this.name = productUpdateRequestDto.getName();
+        this.description = productUpdateRequestDto.getDescription();
+        this.stock = productUpdateRequestDto.getStock();
+        this.category = productUpdateRequestDto.getCategory();
+        this.price = productUpdateRequestDto.getPrice();
+    }
+
+}
