00f3aca0cff39c58aef3ce1e02c6b18447ea69ab
gnoesnooj <306yyy@naver.com>
2022-12-22 18:04:53 +0800

    상세조회 구현
    - enum을 통한 조회 메소드 구현
    - 상세조회 dto 추가
    - controller 조회 코드 구현
    - repository 조회 메소드 추가
    - service 조회 코드 구현

--- src/main/java/com/daily/gaboja/product/service/ProductService.java
+++ src/main/java/com/daily/gaboja/product/service/ProductService.java
@@ -3,6 +3,7 @@ package com.daily.gaboja.product.service;
 import com.daily.gaboja.product.domain.Product;
 import com.daily.gaboja.product.dto.ProductRequestDto;
 import com.daily.gaboja.product.dto.ProductResponseDto;
+import com.daily.gaboja.product.dto.ProductSearchRequestDto;
 import com.daily.gaboja.product.exception.NoSuchProductExist;
 import com.daily.gaboja.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -47,9 +48,16 @@ public class ProductService {
 
     public List<ProductResponseDto> readAll() {
         List<Product> products = productRepository.findAll();
-        List<ProductResponseDto> productResponsDtos = products.stream()
+        return products.stream()
                 .map(product -> new ProductResponseDto().toDto(product))
                 .toList();
-        return productResponsDtos;
+    }
+
+    public List<ProductResponseDto> search(ProductSearchRequestDto productSearchRequestDto){
+        ProductSearcher productSearcher = ProductSearcher.valueOf(productSearchRequestDto.getType());
+        List<Product> products = productSearcher.findProductByKeyword(productRepository, productSearchRequestDto.getKeyword());
+        return products.stream()
+                .map(product -> new ProductResponseDto().toDto(product))
+                .toList();
     }
 }
