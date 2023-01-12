3530cd6a140bb3634884af331d37a4967ecc186b
gnoesnooj <306yyy@naver.com>
2023-01-12 18:20:18 +0800

    product 테스트코드 수정

--- src/test/java/com/daily/gaboja/product/service/ProductServiceTest.java
+++ src/test/java/com/daily/gaboja/product/service/ProductServiceTest.java
@@ -58,64 +58,6 @@ class ProductServiceTest {
     }
 
     @Test
-    void 상품_조회_성공() {
-        Product product = Product.builder()
-                .id(1L)
-                .name("Test")
-                .description("Test")
-                .stock(100L)
-                .category("Test")
-                .price(1000)
-                .build();
-
-        given(productRepository.findById(anyLong())).willReturn(Optional.ofNullable(product));
-
-        ProductResponseDto foundProduct = productService.readOne(product.getId());
-
-        assertThat(foundProduct).isNotNull();
-        assertThat(foundProduct.getId()).isEqualTo(product.getId());
-        assertThat(foundProduct.getName()).isEqualTo(product.getName());
-    }
-
-    @Test
-    void 상품_조회_실패() {
-        given(productRepository.findById(anyLong())).willReturn(Optional.empty());
-
-        assertThatThrownBy(() -> productService.readOne(1L)).isInstanceOf(NoSuchProductExist.class);
-    }
-
-    @Test
-    void 상품_전체조회_성공() {
-        Product product1 = Product.builder()
-                .id(2L)
-                .name("Test2")
-                .description("Test2")
-                .stock(100L)
-                .category("Test2")
-                .price(1000)
-                .build();
-
-        Product product2 = Product.builder()
-                .id(3L)
-                .name("Test3")
-                .description("Test3")
-                .stock(100L)
-                .category("Test3")
-                .price(1000)
-                .build();
-
-        List<Product> foundList = new ArrayList<>();
-        foundList.add(product1);
-        foundList.add(product2);
-
-        given(productRepository.findAll()).willReturn(foundList);
-
-        List<ProductResponseDto> productResponseDtoList = productService.readAll();
-
-        assertThat(productResponseDtoList.size()).isEqualTo(2);
-    }
-
-    @Test
     void 상품_삭제_성공() {
         Product product1 = Product.builder()
                 .id(2L)
@@ -169,6 +111,7 @@ class ProductServiceTest {
 
         given(productRepository.findById(anyLong())).willReturn(Optional.empty());
 
-        assertThatThrownBy(() -> productService.update(1L, productUpdateRequestDto)).isInstanceOf(NoSuchProductExist.class);
+        assertThatThrownBy(() -> productService.update(1L, productUpdateRequestDto)).isInstanceOf(
+                NoSuchProductExist.class);
     }
 }
